f1da37b1364ae2326617f6cf0d5939627c04d411
mitej23 <mitejmadan@gmail.com>
2021-06-27 18:05:55 +0530

    remove safe area into seperate file

--- src/features/restaurants/screens/restaurant.screen.js
+++ src/features/restaurants/screens/restaurant.screen.js
@@ -1,14 +1,11 @@
 /* eslint-disable */
 import React from "react";
 import styled from "styled-components/native";
-import { FlatList, SafeAreaView, StatusBar } from "react-native";
+import { FlatList } from "react-native";
 import { Searchbar } from "react-native-paper";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+import { SafeArea } from "../../../components/utility/safe-area.component";
 
-const SafeArea = styled(SafeAreaView)`
-  flex: 1;
-  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px `};
-`;
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
